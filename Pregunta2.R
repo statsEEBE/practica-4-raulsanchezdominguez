e9ed37e77587fc14832c4ab95c9febf270989b5c
raul.sanchez.dominguez <raul.sanchez.dominguez@auleseebe.upc.edu>
2025-03-25 09:53:00 +0100

    pregunta 2 solucion

--- Pregunta2.R
+++ Pregunta2.R
@@ -1,1 +1,22 @@
+#pregunta 2
+#a
+lambda=1/10000
+1-pexp(13000,lambda)
+x<-0:50
+Fx<- pexp(x,lambda)
+plot(x,Fx)
 
+#b
+qexp(0.8,lambda)
+
+#c
+set.seed(85)
+simul<- rexp(100,lambda)
+hist(simul)
+mean(simul)
+
+#d
+median(simul)
+
+#e
+var(simul)
